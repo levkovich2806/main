7418f163d1abc59963e82c837bd3b60eba41186f
Sergey Levkovich <sergey.levkovich@rdigital.by>
2020-07-05 14:30:03 +0300

    changes

--- layouts/main/index.jsx
+++ layouts/main/index.jsx
@@ -6,14 +6,14 @@ import styles from './index.module.css';
 const withAppLayout = (PageContent) => {
   return (props) => {
     return (
-      <>
+      <div className={styles.main}>
         <div className={styles.menu}>
           <Menu/>
         </div>
         <div className={styles.content}>
           <PageContent {...props} />
         </div>
-      </>
+      </div>
     )
   }
 }
