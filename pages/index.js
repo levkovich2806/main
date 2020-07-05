4c0f249c61da1b51939de89df80fc574fc9ebf5d
Sergey Levkovich <sergey.levkovich@rdigital.by>
2020-07-05 11:07:33 +0300

    changes

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import Head from 'next/head';
-import levkovich from '../public/levkovich.png';
+import withAppLayout from '../layouts/main';
 
-export default function Home() {
+import styles from './index.module.css';
+import Menu from "../components/menu";
+
+function Home() {
   return (
     <div className="container">
       <Head>
@@ -10,118 +13,14 @@ export default function Home() {
         {/*<link rel="icon" href="/favicon.ico" />*/}
       </Head>
 
-      <main>
-        <img src={levkovich} alt={"Sergey Levkovich"} className={"levkovich"}/>
-        <h1 className="title">
-          Sergey Levkovich
-        </h1>
-
+      <main className={styles.container}>
+        <div className={styles.main}>
+          Hello, I'm <span className={styles.author}>Sergey Levkovich</span>.<br/>
+          I'm a web developer.
+        </div>
       </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo"/>
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 4rem;
-          font-size: 3rem;
-        }
-
-        .title {
-          text-align: center;
-        }
-        
-        .levkovich {
-          width: 200px;
-          border-radius: 10% 50% 10% 10%;
-          
-        }
-        
-        .levkovich:hover {
-          box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
-        }
-
-        .logo {
-          height: 1em;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   )
 }
+
+export default withAppLayout(Home);
