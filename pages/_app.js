2385b99102bcddffd44c7ab1b816c342ebd17f0b
sergey <levkovich2806@gmail.com>
2022-09-04 17:08:31 +0200

    Ymetrika was removed

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import React, {useEffect} from "react"
 import Head from 'next/head'
-import { YMInitializer } from 'react-yandex-metrika'
 import '../styles.css'
 
 // This default export is required in a new `pages/_app.js` file.
@@ -10,9 +9,6 @@ export default function MyApp({Component, pageProps}) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover"/>
       </Head>
-      {process.env.NODE_ENV !== 'development' && (
-        <YMInitializer accounts={[73370383]} options={{webvisor: true}} version="2"/>
-      )}
       <div style={{height: "100vh", width: "100vw"}}><Component {...pageProps} /></div>
     </>
   )
