a20b4aabd5399be3483f6870e534928e3a7f2ce3
Sergey Levkovich <s.levkovich@sdventures.com>
2025-10-23 21:10:04 +0200

    change design

--- components/YuoTubeVideo/index.tsx
+++ components/YuoTubeVideo/index.tsx
@@ -1,4 +1,4 @@
-import React, {memo} from "react";
+import React, { memo } from "react";
 
 type Props = {
     embedLink: string
@@ -7,13 +7,19 @@ type Props = {
 export const YuoTubeVideo = memo((props: Props) => {
     return (
         <iframe
-            width="560"
+            width="100%"
             height="315"
             src={props.embedLink}
             title="Optimizing React: Web & Beyond"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowFullScreen>
+            allowFullScreen
+            style={{
+                maxWidth: '560px',
+                borderRadius: '15px',
+                boxShadow: '0 10px 30px rgba(0, 0, 0, 0.3)',
+                transition: 'transform 0.3s ease, box-shadow 0.3s ease'
+            }}>
         </iframe>
     )
 })
